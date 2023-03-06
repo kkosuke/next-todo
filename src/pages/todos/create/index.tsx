a70fd48c032b014d63b223d39dfaf343704e5db6
kkosuke <kkosuke007@gmail.com>
2023-03-06 20:57:21 +0900

    todo作成時に遷移する

--- src/pages/todos/create/index.tsx
+++ src/pages/todos/create/index.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { Button } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Snackbar,
+  TextareaAutosize,
+  TextField,
+} from "@mui/material";
 import {
   collection,
   addDoc,
@@ -9,19 +16,15 @@ import {
   Timestamp,
 } from "firebase/firestore";
 import { db } from "@/lib/firebase";
+import { PrimaryLinkButton } from "@/components/atoms/button/PrimaryLinkButton";
+import { useRouter } from "next/router";
 
 const Create = () => {
+  const router = useRouter();
   const [todoTitle, setTodoTitle] = useState("");
   const [todoDetail, setTodoDetail] = useState("");
-  const [todoDeadlineAt, setTodoDeadlineAt] = useState("2023-03-01T00:00"); // 暫定的に設定（設定 or 未設定ができれば良いが…）
 
-  // 一旦コメントアウトで保存しておく。
-  // const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-  //   setTodoTitle(e.target.value);
-  // ;
-  // const handleDetailChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
-  //   setTodoDetail(e.target.value);
-  // ;
+  const [todoDeadlineAt, setTodoDeadlineAt] = useState("2023-03-01T00:00"); // 暫定的に設定（設定 or 未設定ができれば良いが…）
 
   // 型の参考：https://qiita.com/Takepepe/items/f1ba99a7ca7e66290f24
   const handelFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -37,6 +40,13 @@ const Create = () => {
       console.log("Document written with ID: ", docRef.id);
       setTodoTitle("");
       setTodoDetail("");
+      router.push(
+        {
+          pathname: "/todos",
+          query: { situation: "todo_creat_success" },
+        },
+        "/"
+      );
     } else {
       alert("タイトルを入力してください。");
     }
@@ -49,35 +59,48 @@ const Create = () => {
       </Head>
       <h1>todos/create</h1>
       <main>
-        <div>
-          <Link href="/todos">TODO一覧に戻る</Link>
-        </div>
+        <p>
+          <PrimaryLinkButton
+            href="/todos"
+            text="TODO一覧に戻る"
+            variant="outlined"
+          />
+        </p>
         <form onSubmit={handelFormSubmit}>
-          <div>タイトル</div>
-          <div>
-            <input
-              type="text"
+          <Box>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="TODOのタイトル"
               placeholder="例）今日のご飯"
+              type="text"
               value={todoTitle}
               onChange={(e) => setTodoTitle(e.target.value)}
             />
-          </div>
-          <div>詳細</div>
-          <div>
-            <textarea
+          </Box>
+          <Box mt={2}>
+            <TextField
+              multiline
+              fullWidth
               value={todoDetail}
+              InputProps={{
+                rows: 3,
+              }}
+              label="TODOの詳細"
               placeholder="例）生姜焼き、味噌汁、ごはん"
               onChange={(e) => setTodoDetail(e.target.value)}
-            ></textarea>
-          </div>
-          <div>期限</div>
-          <div>
-            <input
+            />
+          </Box>
+          <Box mt={2} mb={2}>
+            <TextField
+              label="TODOの期限"
               type="datetime-local"
               value={todoDeadlineAt}
               onChange={(e) => setTodoDeadlineAt(e.currentTarget.value)}
             />
-          </div>
+          </Box>
           <Button variant="contained" type="submit">
             この内容で作成する
           </Button>
